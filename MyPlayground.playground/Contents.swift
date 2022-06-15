b5d5eadb47e44b7dead49adb6e754de2cf51e958
futa29 <fu50ta@ezweb.ne.jp>
2022-06-15 19:28:35 +0900

    6/15の分

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -147,25 +147,149 @@ import CoreGraphics
 
 
 //応用問題１　valuesとtargetを受け取り、配列から二つの値の和がtargetとなる組み合わせを表示
-func twoSums(from values: [Int], target: Int) -> [Int] {
-    var x = 0
-        for i in x..<values.count {
-            if target == (values[i] + values [i+1]){
-                let list = [values[i],values [i+1]]
-                return list
-            }
-            x += 1
-            if x == values.count-1{
-                return []
-            }
-        }
+//func twoSums(from values: [Int], target: Int) -> [Int] {
+//    var x = 0
+//        for i in x..<values.count {
+//            if target == (values[i] + values [i+1]){
+//                let list = [values[i],values [i+1]]
+//                return list
+//            }
+//            x += 1
+//            if x == values.count-1{
+//                return []
+//            }
+//        }
+//
+//    return [0]
+//}
+//
+//let values = [3, 2, 9, 1, 10]
+//
+//let result1 = twoSums(from: values, target: 11)
+//print("result1: \(result1)")
+//let result2 = twoSums(from: values, target: 8)
+//print("result2: \(result2)")
+
+
+//フィボナッチ数列
+//func getFibonacci(limit: Int) -> [Int] {
+//    var list = [0,1]
+//    for x in 0..<limit {
+//        let sum = list[x] + list[x+1]
+//        if sum >= limit {
+//            break
+//        }
+//        list.append(list[x] + list[x+1])
+//
+//    }
+//
+//
+//    return list
+//}
+//
+//print(getFibonacci(limit: 1000))
+
+
+//問題for in が若干難しい
+//func sort(list: [Int]) -> [Int] {
+//    var num = 0
+//    var result = list
+//    for x in 0..<list.count{
+//        for y in x+1..<list.count{
+//            if result[x]<result[y]{
+//                num = result[y]
+//                result[y] = result[x]
+//                result[x] = num
+//            }
+//        }
+//
+//    }
+//    return result
+//}
+//
+//let list = [10, 11, 5, 15, 9, 2, 5]
+//let sortedList = sort(list: list)
+//
+//print(sortedList)
+
+
+//九九表
+//func printMultiplicationTable() {
+//    for x in 1...9{
+//        for y in 1...9{
+//            print(x*y,terminator: " 　")
+//        }
+//        print("")
+//    }
+//}
+//printMultiplicationTable()
+
+
+//func getFibonacci(limit: Int) -> [Int] {
+//    var list = [0,1]
+//    for x in 0..<limit {
+//        let sum = list[x] + list[x+1]
+//        if sum >= limit {
+//            break
+//        }
+//        list.append(list[x] + list[x+1])
+//
+//    }
+//
+//
+//    return list
+//}
+//
+//print(getFibonacci(limit: 1000))
+
+
+
+
+//構造体定義
+//struct Article {
+//  //ストアドプロパティ定義（初期値なし）
+//  let id: Int
+//  let title: String
+//  let body: String
+//
+//  //initを使用することでイニシャライザ定義　javaで言うコンストラクタ
+////  init(id: Int, title: String, body: String) {
+////    self.id = id
+////    self.title = title
+////    self.body = body
+////  }
+//
+//  func printBody() {
+//    print(body)
+//    print(id)
+//    print(title)
+//  }
+//}
+//
+////インスタンス化時に引数に値を置くことで、イニシャライザで初期値を与える
+//let article = Article(id: 2, title: "test", body: "body")
+//article.printBody()
+
+
+
+//var str : String? = "hoge"
+//var str2 : String? = nil
+//
+//print(str?.uppercased())
+//print(str2)
+
+
+
+
+var city = 0
+var city2 = "アイウエオ"
+print(city)
+print(city2)
     
-    return [0]
-}
 
-let values = [3, 2, 9, 1, 10]
 
-let result1 = twoSums(from: values, target: 11)
-print("result1: \(result1)")
-let result2 = twoSums(from: values, target: 8)
-print("result2: \(result2)")
+
+
+
+
+
